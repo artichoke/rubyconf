f778e6682a04e2ec2e6f46d60ac7d9ab81676b98
Ryan Lopopolo <rjl@hyperbo.la>
2021-12-03 09:39:55 -0800

    Rewrite sources to compile with esbuild

--- src/landing.js
+++ src/landing.js
@@ -1,11 +1,3 @@
 import "reveal.js/dist/reset.css";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/white.css";
-
-// Assets with well-known filenames
-import "@artichokeruby/logo/img/artichoke-logo.png";
-import "@artichokeruby/logo/img/artichoke-logo.svg";
-import "@artichokeruby/logo/img/artichoke-logo-inverted.png";
-import "@artichokeruby/logo/img/artichoke-logo-inverted.svg";
-import "@artichokeruby/logo/img/artichoke-social-logo.png";
-import "@artichokeruby/logo/img/artichoke-social-profile.png";
